48548d73dd48b477d6be5864bbae14a9f21ed2b3
marinaerokhina <erokhina.marina.2016@mail.ru>
2021-05-05 12:56:53 +0700

    xrx

---
+++ app/src/main/java/com/example/passactivity/Person.java
@@ -0,0 +1,79 @@
+package com.example.passactivity;
+
+import android.widget.EditText;
+
+public class Person {
+    private String Login;
+    private String Password;
+    private String Name;
+    private String Surname;
+    private String Status;
+    private String Grade;
+    private int Age;
+
+    public Person(String login, String password, String name, String surname, String status, String grade, int age) {
+        Login = login;
+        Password = password;
+        Name = name;
+        Surname = surname;
+        Status = status;
+        Grade = grade;
+        Age = age;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public void setGrade(String grade) {
+        Grade = grade;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public String getGrade() {
+        return Grade;
+    }
+}
