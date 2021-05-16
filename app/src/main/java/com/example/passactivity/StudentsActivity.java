c524c497827f6dafeb90def1ee8687d245d4eb88
marinaerokhina <erokhina.marina.2016@mail.ru>
2021-05-16 20:03:59 +0700

    xrx

--- app/src/main/java/com/example/passactivity/StudentsActivity.java
+++ app/src/main/java/com/example/passactivity/StudentsActivity.java
@@ -4,7 +4,6 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
